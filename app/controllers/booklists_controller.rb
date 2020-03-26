3cf601a7d1b3c5b730b2ca433c90c618c8e6d711
vagrant <takakoba0426@gmail.com>
2020-03-26 04:30:17 +0000

    add

--- app/controllers/booklists_controller.rb
+++ app/controllers/booklists_controller.rb
@@ -10,10 +10,11 @@ class BooklistsController < ApplicationController
          @book = Book.new(book_params)
         # DBへ保存する
          if @book.save
-
+         	flash[:notice] = "Book was successfully created."
         redirect_to booklist_path(@book.id)
     else
-    	render action: :new
+    	@books = Book.all
+    	render action: :index
      end
     end
 
@@ -24,9 +25,14 @@ class BooklistsController < ApplicationController
     	@book = Book.find(params[:id])
     end
     def update
-    	book = Book.find(params[:id])
-    	book.update(book_params)
-    	redirect_to booklist_path(book.id)
+    	@book = Book.find(params[:id])
+    	if @book.update(book_params)
+    		flash[:notice] = "Book was successfully updated."
+    	redirect_to booklist_path(@book.id)
+    else
+    	@books = Book.all
+    	render action: :edit
+    end
     end
     def destroy
     	book = Book.find(params[:id])
